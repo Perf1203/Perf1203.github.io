bec896d803aa8dae1d31e25b2657cbd0743aa50d
Pedro Figueredo <105466107+Perf1203@users.noreply.github.com>
2023-01-25 16:13:26 +0200

    add file

---
+++ agapecoin4all.com/cuex/default/db login/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login</title>
+</head>
+<body>
+<h2>Sistema de Login</h2>
+  <form action="validar.php" method="POST">
+  <p><label for="nickname">User</label>  
+  <input type="text" name="nickname"></p>
+  
+  <p><label for="pasword">Password</label>  
+  <input type="text" name="pasword"></p>
+
+  <p><input type="submit" value="Acceder"></p>
+
+  </form>  
+</body>
+</html>
